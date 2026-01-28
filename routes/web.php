612894e84c4339aac3fa970b133d683fd4b6f550
albin <albinguignabert@gmail.com>
2026-01-28 21:29:23 +0100

    feat: add chirp controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChripController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ChripController::class,'index']);
