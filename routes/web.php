7828be18d33f3dcbb5c2546f28e5ee6a00369d80
albin <albinguignabert@gmail.com>
2026-02-02 17:47:44 +0100

    feat: update/delete

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChripController::class, 'index']);
 Route::post('/chirps', [ChripController::class, 'store']);
+Route::get('/chirps/{chirp}/edit', [ChripController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChripController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChripController::class, 'destroy']);
